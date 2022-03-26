90b379b63419c180297fc65baea209efea86de6c
shrey8888 <100024327+shrey8888@users.noreply.github.com>
2022-03-26 15:52:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+var box1;
+var box2;
+function setup() {
+  createCanvas(600, 400);
+  box1 = new Box()
+box2 = new Box()
+
+
+}
+
+function draw() {
+  background(220);
+box1.set_width(100)
+box2.show()
+box1.show()
+
+box2.set_width(200)
+
+}
+
+
